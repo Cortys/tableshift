583a2ab496a6f60aa4e2863563c25cee5ab6a29b
Clemens Damke <damke@kiml.ifi.lmu.de>
2026-02-17 03:56:55 +0100

    added pyproject.toml + made compatible with python 3.13

--- tableshift/core/utils.py
+++ tableshift/core/utils.py
@@ -9,7 +9,6 @@ from itertools import islice
 
 import pandas as pd
 import requests
-import xport.v56
 
 from .splitter import Splitter, DomainSplitter
 
@@ -26,8 +25,7 @@ def basename_from_url(url: str) -> str:
     return fname
 
 
-def download_file(url: str, dirpath: str, if_not_exist=True,
-                  dest_file_name=None):
+def download_file(url: str, dirpath: str, if_not_exist=True, dest_file_name=None):
     """Download file to the specified directory."""
     if dest_file_name:
         fname = dest_file_name
@@ -50,17 +48,7 @@ def download_file(url: str, dirpath: str, if_not_exist=True,
 
 def read_xpt(fp) -> pd.DataFrame:
     assert os.path.exists(fp), "file does not exist %s" % fp
-    with open(fp, "rb") as f:
-        obj = xport.v56.load(f)
-    # index into SAS structure, assuming there is only one dataframe
-    assert len(tuple(obj._members.keys())) == 1
-    key = tuple(obj._members.keys())[0]
-    ds = obj[key]
-    # convert xport.Dataset to pd.DataFrame
-    columns = [ds[c] for c in ds.columns]
-    df = pd.DataFrame(columns).T
-    del ds
-    return df
+    return pd.read_sas(fp, format="xport")  # ty:ignore[invalid-return-type]
 
 
 def run_in_subprocess(cmd):
@@ -71,7 +59,7 @@ def run_in_subprocess(cmd):
 
 
 def sliding_window(iterable, n):
-    """ From https://docs.python.org/3/library/itertools.html"""
+    """From https://docs.python.org/3/library/itertools.html"""
     # sliding_window('ABCDEFG', 4) --> ABCD BCDE CDEF DEFG
     it = iter(iterable)
     window = collections.deque(islice(it, n), maxlen=n)
@@ -86,19 +74,19 @@ def make_uid(name: str, splitter: Splitter, replace_chars="*/:'$!") -> str:
     """Make a unique identifier for an experiment."""
     uid = name
     if isinstance(splitter, DomainSplitter) and splitter.is_explicit_split():
-        attrs = {'domain_split_varname': splitter.domain_split_varname,
-                 'domain_split_ood_value': ''.join(
-                     str(x) for x in splitter.domain_split_ood_values)}
+        attrs = {
+            "domain_split_varname": splitter.domain_split_varname,
+            "domain_split_ood_value": "".join(str(x) for x in splitter.domain_split_ood_values),
+        }
         if splitter.domain_split_id_values:
-            attrs['domain_split_id_values'] = ''.join(
-                str(x) for x in splitter.domain_split_id_values)
-        uid += ''.join(f'{k}_{v}' for k, v in attrs.items())
+            attrs["domain_split_id_values"] = "".join(str(x) for x in splitter.domain_split_id_values)
+        uid += "".join(f"{k}_{v}" for k, v in attrs.items())
     elif isinstance(splitter, DomainSplitter) and splitter.is_threshold_split():
-        uid += f'{splitter.domain_split_varname}gt{splitter.domain_split_gt_thresh}'
+        uid += f"{splitter.domain_split_varname}gt{splitter.domain_split_gt_thresh}"
 
     # if any slashes exist, replace with periods.
     for char in replace_chars:
-        uid = uid.replace(char, '.')
+        uid = uid.replace(char, ".")
     # Max path length on some OSs is 255.
     return uid[:240]
 
@@ -127,6 +115,6 @@ def convert_64bit_numeric_cols(df: pd.DataFrame) -> pd.DataFrame:
     """Convert float64 or int64 columns to float/int32.
 
     Not all numpy dtypes are compatible with 64-bit precision."""
-    int64_float64_cols = list(df.select_dtypes(include=['float64', 'int64']))
-    df[int64_float64_cols] = df[int64_float64_cols].astype('float32')
+    int64_float64_cols = list(df.select_dtypes(include=["float64", "int64"]))
+    df[int64_float64_cols] = df[int64_float64_cols].astype("float32")
     return df
